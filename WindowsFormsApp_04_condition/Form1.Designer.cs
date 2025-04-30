0da3f9860b2cfd12f44dd32144529666f2d0343c
CHK404 <azbxabcd4@gmail.com>
2025-04-30 10:46:18 +0900

    Add project files.

---
+++ WindowsFormsApp_04_condition/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp_04_condition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
+            this.tButton = new System.Windows.Forms.RadioButton();
+            this.fButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Location = new System.Drawing.Point(31, 184);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(648, 314);
+            this.textBox_result.TabIndex = 0;
+            this.textBox_result.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(31, 56);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(417, 71);
+            this.textBox_input.TabIndex = 1;
+            this.textBox_input.TextChanged += new System.EventHandler(this.textBox_input_TextChanged);
+            // 
+            // button_input
+            // 
+            this.button_input.Font = new System.Drawing.Font("Gulim", 15F);
+            this.button_input.Location = new System.Drawing.Point(464, 56);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(215, 71);
+            this.button_input.TabIndex = 2;
+            this.button_input.Text = "Input";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // tButton
+            // 
+            this.tButton.AutoSize = true;
+            this.tButton.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tButton.Location = new System.Drawing.Point(31, 147);
+            this.tButton.Name = "tButton";
+            this.tButton.Size = new System.Drawing.Size(72, 24);
+            this.tButton.TabIndex = 3;
+            this.tButton.TabStop = true;
+            this.tButton.Text = "True";
+            this.tButton.UseVisualStyleBackColor = true;
+            this.tButton.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // fButton
+            // 
+            this.fButton.AutoSize = true;
+            this.fButton.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fButton.Location = new System.Drawing.Point(141, 147);
+            this.fButton.Name = "fButton";
+            this.fButton.Size = new System.Drawing.Size(79, 24);
+            this.fButton.TabIndex = 4;
+            this.fButton.TabStop = true;
+            this.fButton.Text = "False";
+            this.fButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1421, 599);
+            this.Controls.Add(this.fButton);
+            this.Controls.Add(this.tButton);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.textBox_result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.RadioButton tButton;
+        private System.Windows.Forms.RadioButton fButton;
+    }
+}
+
---
+++ WindowsFormsApp_04_condition/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_04_condition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //#region 이란
+            //ㄴ VS 특정 코드 블록을 접어서 숨기게 만들어주는 기능
+            //ㄴ #endregion으로 꼭 닫아주기
+
+            #region #1 if문 강의
+            int inputNum = 10;
+
+            if(inputNum > 20)
+            {
+                //inputNum은 20보다 크지 않음 -> false
+            }
+            else if (inputNum < 5) 
+            {
+                //inputNum은 20보다 크지 않고, 5보다 작지 않음 -> false
+            }
+            else if (inputNum == 8)
+            {
+                //inputNum은 20보다 크지 않고, 5보다 작지 않으며 8도 아님 -> false
+            }
+            else
+            {
+                //위 조건이 모두 false 이므로 -> true
+            }
+
+            //조건과 조건을 비교하는 연산 수행
+            bool is_true = inputNum == 10; //true
+
+            //(조건1) or (조건2)
+            //단축평가 통해
+            bool compared = (inputNum > 10 || inputNum < 5); // false
+
+            #endregion
+
+            #region #1-2 실습 if문)
+            /*if (CoinMatch(true))
+             *{
+             *   textBox_result.Text = "승리";
+             *}
+             *else
+             *{
+             *   textBox_result.Text = "패배";
+             */
+            #endregion
+        }
+        #region #1-2 실습 if문에 사용되는 함수
+        bool CoinMatch(bool type)
+        {
+            //난수를 생성하기 위한 클래스
+            //ㄴ 기본적으로 현재 시간을 기준으로 시드 자동 생성
+            Random randomObj = new Random();
+            /*
+             * Random: 클래스
+             * new Random(): Random이라는 클래스를 이용해서 'randomObj'라는 객체를 만든 것
+             * randomObj: 객체
+             */
+
+            //짝수/홀수로 나눠서 0 혹은 1로 결정하게 하기
+            int coin = randomObj.Next() % 2; //rnd.Next(): 0보다 크고 매우 큰 int값을 반환
+            /* 
+             * rnd.Next(): 0이상 int.MaxValue 미만 까지(약 21억)
+             * rnd.Next(max): 0이상 max 미만
+             * rnd.Next(min, max): min 이상 max 미만
+             * rnd.NextDouble(): 0.0이상 1.0미만 소수를 랜덤 생성
+             * -Random은 재사용이 좋다
+             */
+
+            /*
+             * Math(=내장 클래스)
+             * Math.Round(): 반올림
+             * Math.Floor(): 내림
+             * Math.Ceiling(): 올림
+             */
+
+            if((coin == 1 && type == true) || (coin == 0 && type == false)) 
+            { 
+                return true;
+            }
+            //1이면 앞면(true), 0이면 뒷면(false)
+            //동전 결과(coin)와 사용자의 선택(type)이 일치할 경우에만 true 반환
+            return false;   //false 반환: 이 함수의 결과가 일치 x, else 사용해도 무방
+            #endregion
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            if ((textBox_input.Text == "true") && (CoinMatch(true)))
+            {
+                textBox_result.Text = "입력하신 값은 true입니다\r\n동전 던지기 결과..\r\n승리";
+            }
+            else if ((textBox_input.Text == "false") && (CoinMatch(true)))
+            {
+                textBox_result.Text = "입력하신 값은 false입니다\r\n동전 던지기 결과..\r\n패배";
+            }
+            else if ((textBox_input.Text == "false") && (CoinMatch(false)))
+            {
+                textBox_result.Text = "입력하신 값은 false입니다\r\n동전 던지기 결과..\r\n승리";
+            }
+            else if ((textBox_input.Text == "true") && (CoinMatch(false)))
+            {
+                textBox_result.Text = "입력하신 값은 true입니다\r\n동전 던지기 결과..\r\n패배";
+            }
+            else
+            {
+                textBox_result.Text = "잘못된 입력입니다. 다시 입력해주세요.";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox_input_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (tButton.Checked == true)
+            {
+                textBox_input.Text = "true";
+            } else if (fButton.Checked == true)
+            {
+                textBox_input.Text = "false";
+            }
+
+        }
+    }
+}
